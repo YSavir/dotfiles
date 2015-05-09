b480ac0f0a131c652e0d58079e784a75d1a31be9
Yaniv Savir <saviry@gmail.com>
2015-05-09 17:05:28 -0400

    rspec backtrace only ignores /gems/ in ruby directory

--- .rspec-setup.rb
+++ .rspec-setup.rb
@@ -1,6 +1,6 @@
 # require 'database_cleaner'
 
 RSpec.configure do |config|
-  config.backtrace_exclusion_patterns << /gems/
+  config.backtrace_exclusion_patterns << /ruby-2\.2\.1\/gems/
   config.fail_fast = true
 end
