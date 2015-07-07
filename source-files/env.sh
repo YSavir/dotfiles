100437ca014d19a24a9e818b9a9d45896216d6d1
Yaniv Savir <saviry@gmail.com>
2015-07-07 11:44:58 -0400

    adds rbenv

--- source-files/env.sh
+++ source-files/env.sh
@@ -5,3 +5,12 @@ export APP_ENV="development"
 
 alias serveThis='ruby -run -e httpd . -p 9090'
 
+### RBENV
+
+export RBENV_ROOT="${HOME}/.rebenv"
+
+if [ -d $RBENV_ROOT ]; then
+  export PATH="${PATH}:${RBENV_ROOT}/bin"
+  eval $(rbenv init -)"
+fi
+  
