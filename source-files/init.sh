98bf619cceaf38dae8146b0f3aacd8dbf82a0b1d
Yaniv Savir <saviry@gmail.com>
2016-01-29 15:12:40 -0500

    add work.sh to init flow

--- source-files/init.sh
+++ source-files/init.sh
@@ -4,10 +4,15 @@ source_files='env '\
 'utils '\
 'fetchers '\
 'path '\
-'prompt'
+'prompt '\
+'work'
 
 for source_file in $source_files; do
-  source ~/dotfiles/source-files/$source_file.sh
+  file="~/dotfiles/source-files/$source_file.sh"
+  if [ -e "$file" ]
+  then
+    source "$file"
+  fi
 done 
 
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
