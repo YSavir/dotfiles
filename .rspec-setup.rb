83680d223a94ebbc9b5fd6bcb18632872220b722
Yaniv Savir <yaniv@yanivsavir.com>
2020-10-11 11:59:43 -0400

    add rspec defaults

--- .rspec-setup.rb
+++ .rspec-setup.rb
@@ -3,4 +3,6 @@
 RSpec.configure do |config|
   config.backtrace_exclusion_patterns << /ruby-2\.2\.1\/gems/
   config.fail_fast = true
+  config.filter_run focus: true
+  config.run_all_when_everything_filtered = true
 end
